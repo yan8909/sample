328d93e8613c28b0d77ac6645f1b72bd62176931
yan8909 <yan8909@gmail.com>
2018-12-27 08:02:49 +0000

    follow button

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -19,6 +19,11 @@ class UserPolicy
         return $currentUser->is_admin && $currentUser->id !== $user->id;
     }
 
+    public function follow(User $currentUser, User $user)
+    {
+        return $currentUser->id !== $user->id;
+    }
+
     // public function __construct()
     // {
     //     //
