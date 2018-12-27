d6060512d67bd1c42f5b5ec32eb6ca23dff05b06
jerry <yan8909@gmail.com>
2018-12-27 06:22:28 +0000

    weibo data model

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable
     {
         $this->notify(new ResetPassword($token));
     }
+
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
 }
